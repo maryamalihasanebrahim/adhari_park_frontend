3aeefa7ebfbce02e8f5c54b6b586f90e732d8b4b
maryamalihasanebrahim <maryamalihasanebrahim@gmail.com>
2025-08-04 22:14:48 +0300

    ride routes (backup) booking form front end done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import "./App.css"
-
+import './App.css'
+import Form from './components/Booking'
 function App() {
-  return <></>
+  return (
+    <>
+      <Form />
+    </>
+  )
 }
 
 export default App
