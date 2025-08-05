b4e89155172b60235fc4fdfaa9f90d580fee22b5
Yara-Waleed <yarawaleed292@gmail.com>
2025-08-05 15:40:40 +0300

    view bookings completed

--- src/components/ViewBookings.jsx
+++ src/components/ViewBookings.jsx
@@ -1,5 +1,21 @@
-const ViewBookings = () => {
-  return <></>
-}
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
+const ViewBookings = () => {
+  const [bookings, setBookings] = useState([])
+  useEffect(() => {
+    const onMount = async () => {
+      const response = await axios.get(`http://localhost:3001/bookings`)
+      setBookings(response.data)
+    }
+    onMount()
+  }, [])
+
+  return (
+    <ul>
+      {bookings &&
+        bookings.map((booking) => <li key={booking.id}>{booking.date}</li>)}
+    </ul>
+  )
+}
 export default ViewBookings
