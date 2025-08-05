aee4eabf0f0b955e4223b60baca3f3c2bec77974
alyaalaali <alya.alaali05@gmail.com>
2025-08-05 22:50:48 +0300

    styled auth pages

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import axios from "axios"
-
+import "../styles/auth.css"
 const Register = () => {
   let navigate = useNavigate()
   const initialState = {
