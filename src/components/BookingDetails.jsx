4923c69151997b59bfba39715de7e8ac7f6f3aa5
Yara-Waleed <yarawaleed292@gmail.com>
2025-08-06 12:54:56 +0300

    bookings page styling

--- src/components/BookingDetails.jsx
+++ src/components/BookingDetails.jsx
@@ -1,19 +1,31 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
+import '../styles/Bookings.css'
 
 const BookingDetails = ({ booking, rides }) => {
   return (
     <>
-      <li >
-        {rides.map((ride) => {
-          if (ride._id == booking.rideId){
-          return ride.name.toLowerCase()
-          }
-        })}
-        <br />
-        {booking.date.toString().substring(0, 10)}
-        <Link to={`/Bookings/${booking._id}`}>
-          <p> more details...</p>
-        </Link>
+      <li>
+        <div className="booking-card">
+          {rides.map((ride) => (
+            <div className="rides-box">
+              <div className='ridePic'>
+              <img
+                src="https://lumiere-a.akamaihd.net/v1/images/c94eed56a5e84479a2939c9172434567c0147d4f.jpeg?region=0,0,600,600"
+                alt="anyhting"
+                className="img"
+              />
+              </div>
+              <div className="name"><p>{ride._id === booking.rideId && ride.name.toLowerCase()}</p></div>
+          <br />
+          <div className="date">{booking.date.toString().substring(0, 10)}</div>
+          <Link to={`/Bookings/${booking._id}`}>
+            <div className="moredetails">
+              <p> more details...</p>
+            </div>
+          </Link>
+          </div>
+          ))}
+        </div>
       </li>
     </>
   )
--- src/components/ViewBooking.jsx
+++ src/components/ViewBooking.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
-
+import '../styles/ViewBooking.css'
 const ViewBooking = () => {
   const { id } = useParams()
   const navigate = useNavigate()
@@ -29,14 +29,29 @@ const ViewBooking = () => {
   if (!booking) return <div>loading...</div>
 
   return (
-    <div>
-      <h2>booking details</h2>
-      <p>ride: {rideName}</p>
-      <p>date: {booking.date.toString().substring(0, 10)}</p>
-      <p>time: {booking.time}</p>
-      <p>booking for {booking.persons} person(s)</p>
-      <button onClick={handleDelete}>cancel</button>
-      <button onClick={() => navigate('/bookings')}>back</button>
+    <div className="viewbooking">
+      {rideName === 'Bubble Blaster' && (
+        <img src="\src\images\ride_1.png" alt="Bubble Blaster" className="imageOfRide" />
+      )}
+      {rideName === 'BitCoaster' && (
+        <img src="src/images/ride_2.png" alt="Bit Coaster" className="imageOfRide" />
+      )}
+      {rideName === 'NeoLoop Eye' && (
+        <img src="src/images/ride_3.png" alt="NeoLoop Eye" className="imageOfRide" />
+      )}
+      <div className="bookinginfo">
+        <h2>booking details</h2>
+        <p>ride: {rideName}</p>
+        <p>date: {booking.date.toString().substring(0, 10)}</p>
+        <p>time: {booking.time}</p>
+        <p>booking for {booking.persons} person(s)</p>
+        <div className='cancelbtn'>
+        <button onClick={handleDelete}>cancel</button>
+        </div>
+        <div className='backbtn'>
+        <button onClick={() => navigate('/bookings')}>back</button>
+        </div>
+      </div>
     </div>
   )
 }
