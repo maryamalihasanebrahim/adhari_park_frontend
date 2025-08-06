db1e649325ca6148a797c2689d03571675c2e0ec
alyaalaali <alya.alaali05@gmail.com>
2025-08-06 10:25:35 +0300

    styled ride details

--- src/components/RideDetails.jsx
+++ src/components/RideDetails.jsx
@@ -1,39 +1,39 @@
-import { useParams } from 'react-router-dom'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import { Link } from 'react-router-dom'
-import Form from './Booking'
-import '../styles/Rides.css'
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { Link } from "react-router-dom"
+import Form from "./Booking"
+import "../styles/Rides.css"
 
 const RideDetails = () => {
-  const [chosenRide, setChosenRide] = useState('')
+  const [chosenRide, setChosenRide] = useState("")
   let { id } = useParams()
 
   useEffect(() => {
     const onMount = async () => {
       const response = await axios.get(`http://localhost:3001/rides/${id}`)
 
-      response ? setChosenRide(response.data) : console.log('loading...')
+      response ? setChosenRide(response.data) : console.log("loading...")
     }
     onMount()
   }, [])
 
   return (
     <>
-      <div id="details">
-        <div id='information'>
-        <h3>{chosenRide.name}</h3>
-        <h4>{chosenRide.description}</h4>
-        <p>price: BHD {chosenRide.price}</p>
-        <p>thrill level: {chosenRide.thrill_level}</p>
-        <p>{chosenRide.location}</p>
-        <p>{chosenRide.max_persons} people maximum</p>
-        <p>minimum height {chosenRide.min_height} cm</p>
+      <div className="ride-details">
+        <div className="ride-info">
+          <h3>{chosenRide.name}</h3>
+          <h4>{chosenRide.description}</h4>
+          <p>price: BHD {chosenRide.price}</p>
+          <p>thrill level: {chosenRide.thrill_level}</p>
+          <p>{chosenRide.location}</p>
+          <p>{chosenRide.max_persons} people maximum</p>
+          <p>minimum height {chosenRide.min_height} cm</p>
         </div>
-        <div id='bookingButton'>
-        <Link to={`/bookings/new/${id}`}>
-          <button id="button">book</button>
-        </Link>
+        <div id="bookingButton">
+          <Link to={`/bookings/new/${id}`}>
+            <button className="book-btn">book</button>
+          </Link>
         </div>
       </div>
     </>
